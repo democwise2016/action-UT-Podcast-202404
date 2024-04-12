8cbf6e52ac6eb522a4bc65f16e6154a485296b96
Pulipuli Chen <pulipuli.chen@gmail.com>
2024-04-13 04:28:27 +0800

    西元2024年04月13日 (週六) 04時28分27秒 CST

---
+++ README.md
@@ -0,0 +1,5 @@
+https://www.youtube.com/watch?v=iex9z_Zmzhw
+
+取得ID
+
+https://docs.google.com/spreadsheets/d/1VL7M9rQfIT1yTDx5noSKQFzaEoG2WVUF0k8ODta8Upk/edit#gid=2000612407
---
+++ config-list.js
@@ -0,0 +1,37 @@
+const ItemFiltersPreset = require('./app/config/ItemFiltersPreset.js')
+
+let feedList = [
+  {
+    title: '0號手稿 看ACG',
+    feedID: 'user-soulkao_0',
+    homepageURL: 'https://www.youtube.com/channel/UCbWIKVoPlnahOJj8OZ3kSZA',
+    itemFilters: ItemFiltersPreset.between3minTo30Min,
+  },
+  {
+    title: '紫骷 玩遊戲',
+    feedID: 'Zikukirby',
+    homepageURL: 'https://www.youtube.com/channel/UCQceikp-aRr_e5uCARuPvEQ',
+    itemFilters: ItemFiltersPreset.between3minTo30Min,
+  },
+]
+
+// ---------------------------------------
+
+// 測試用
+// feedList = [
+//   {
+//     title: '萊斯 新Game報',
+//     feedID: 'LiceMoo-GameNews',
+//     homepageURL: 'https://www.youtube.com/channel/UC9WiXJEyHMGRqL-__3FIBEw',
+//     thumbnailBorderColor: true,
+//     itemFilters: [
+//       ItemFiltersPreset.between3minTo30Min,
+//       (item) => { return (item.title.indexOf('《新Game報') > -1) }
+//     ],
+//     options: {
+//       maxItems: 3
+//     }
+//   },
+// ]
+
+module.exports = feedList
