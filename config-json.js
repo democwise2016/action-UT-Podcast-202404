cf3c7438b8f68daddeacdb1e57c3851afe2a89ab
thinkpad-l13-yoga <pulipuli.chen@gmail.com>
2024-08-03 15:19:59 +0800

    西元2024年08月03日 (週六) 15時19分59秒 CST

---
+++ config-json.js
@@ -0,0 +1,21 @@
+let CONFIG = {
+  publicURL: `https://democwise2016.github.io/action-UT-Podcast-202404/`,
+  // publicURL: `https://dangerous-lizard-99.telebit.io/`,
+  
+  publicURLShorten: `https://ppt.cc/fnNzzx`,
+  thumbnailBorderColor: 'B9C6AE',
+
+  titleAppend: '',
+  idAppend: false,
+
+  maxDownloadItemPerFeed: 50,
+  maxDownloadFeed: 50,
+  newArrialMax: 50,
+  maxDownloadItems: 100,
+  maxExcutionMinutes: 25,
+
+  maxFileCache: 5000,
+  // feedList
+}
+
+module.exports = CONFIG
